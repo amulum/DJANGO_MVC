c24148c2fc2c151ae87ff3aa870c756568476924
Achmad Miftahul Ulum <miftahul@alterra.id>
2019-12-09 11:09:55 +0700

    djang make dir practice

---
+++ C/env_project_advance_views/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta1/Documents/alta/phase_1/week_4/dev2/practice/DJANGO_MVC/C/env_project_advance_views/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
